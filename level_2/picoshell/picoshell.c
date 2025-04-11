c40eac7b52923ce5a2e109c562585c8e9c9e5e81
Tatiana <taretiuk@student.42berlin.de>
2025-04-11 09:33:42 +0200

    fixup! fixup! fixup! fixup! Merge branch 'main' of github.com:cr3mebrulee/42_school_exam_rank_04

--- level_2/picoshell/picoshell.c
+++ level_2/picoshell/picoshell.c
@@ -7,98 +7,98 @@
 
 int create_pipes(int fds[][2], int n)
 {
-    for (int i = 0; i < n - 1; i++)
+	for (int i = 0; i < n - 1; i++)
 	{
-        if (pipe(fds[i]) == -1)
+		if (pipe(fds[i]) == -1)
 		{
-            return (-1);
-        }
-    }
-    return (0);
+			return (-1);
+		}
+	}
+	return (0);
 }
 
 // Function to handle the redirection and file descriptor management for each child
 int handle_redirection(int i, int n, int fds[][2]) 
 {
-    // First command: redirect stdout to the first pipe
-    if (i == 0)
+	// First command: redirect stdout to the first pipe
+	if (i == 0)
 	{
-        dup2(fds[i][1], STDOUT_FILENO);
-        close(fds[i][1]);
-    }
-    // Intermediate commands: redirect stdin and stdout to pipes
-    else if (i > 0 && i < n - 1)
+		dup2(fds[i][1], STDOUT_FILENO);
+		close(fds[i][1]);
+	}
+	// Intermediate commands: redirect stdin and stdout to pipes
+	else if (i > 0 && i < n - 1)
 	{
-        dup2(fds[i - 1][0], STDIN_FILENO);  // Read from previous pipe
-        close(fds[i - 1][0]);
-        dup2(fds[i][1], STDOUT_FILENO);     // Write to current pipe
-        close(fds[i][1]);
-    }
-    // Last command: redirect stdin from the previous pipe
-    else if (i == n - 1)
+		dup2(fds[i - 1][0], STDIN_FILENO);  // Read from previous pipe
+		close(fds[i - 1][0]);
+		dup2(fds[i][1], STDOUT_FILENO);     // Write to current pipe
+		close(fds[i][1]);
+	}
+	// Last command: redirect stdin from the previous pipe
+	else if (i == n - 1)
 	{
-        dup2(fds[i - 1][0], STDIN_FILENO);  // Read from previous pipe
-        close(fds[i - 1][0]);
-    }
-    // Close all pipe fds
-    for (int j = 0; j < n - 1; j++)
+		dup2(fds[i - 1][0], STDIN_FILENO);  // Read from previous pipe
+		close(fds[i - 1][0]);
+	}
+	// Close all pipe fds
+	for (int j = 0; j < n - 1; j++)
 	{
-        close(fds[j][0]);
-        close(fds[j][1]);
-    }
-    return (0);
+		close(fds[j][0]);
+		close(fds[j][1]);
+	}
+	return (0);
 }
 
 // Function to fork the child process and execute the command
 int execute_command(char **cmds[], int i, int n, int fds[][2]) 
 {
-    pid_t cpid = fork();
-    if (cpid == 0) 
+	pid_t cpid = fork();
+	if (cpid == 0) 
 	{
-        // Handle redirection
-        if (handle_redirection(i, n, fds) == -1) 
+		// Handle redirection
+		if (handle_redirection(i, n, fds) == -1) 
 		{
-            return (-1);
-        }
-        // Execute command
-        if (execvp(cmds[i][0], cmds[i]) == -1) 
+			return (-1);
+		}
+		// Execute command
+		if (execvp(cmds[i][0], cmds[i]) == -1) 
 		{
-            return (-1);
-        }
-    }
-    return (0);
+			return (-1);
+		}
+	}
+	return (0);
 }
 
 // Main function that sets up the pipe execution
 int execute_in_pipe(char **cmds[], int n) 
 {
-    int fds[n - 1][2];
-    pid_t cpid;
+	int fds[n - 1][2];
+	pid_t cpid;
 
-    // Create pipes
-    if (create_pipes(fds, n) == -1) 
+	// Create pipes
+	if (create_pipes(fds, n) == -1) 
 	{
-        return (-1);
-    }
-    // Execute commands
-    for (int i = 0; i < n; i++) 
+		return (-1);
+	}
+	// Execute commands
+	for (int i = 0; i < n; i++) 
 	{
-        if (execute_command(cmds, i, n, fds) == -1) 
+		if (execute_command(cmds, i, n, fds) == -1) 
 		{
-            return (-1);
-        }
-    }
-    // Parent process: close pipe fds and wait for children
-    for (int i = 0; i < n - 1; i++)
+			return (-1);
+		}
+	}
+	// Parent process: close pipe fds and wait for children
+	for (int i = 0; i < n - 1; i++)
 	{
-        close(fds[i][0]);
-        close(fds[i][1]);
-    }
-    for (int i = 0; i < n; i++) 
+		close(fds[i][0]);
+		close(fds[i][1]);
+	}
+	for (int i = 0; i < n; i++) 
 	{
-        wait(NULL);  // Wait for all children
-    }
-    return (0);
+		wait(NULL);  // Wait for all children
+	}
+	return (0);
 }
 
 int	calculate_cmds(char **cmds[], int *n)
